4f368d4e97bf0bc3415200c2e67b201b5eebda64
alexsandro <alexsandro.nv.02@gmail.com>
2024-10-07 11:22:18 -0300

    alterado rotas

---
+++ api/meli_notifications.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios');
+const serverless = require('serverless-http');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Rota para notificações do Mercado Livre
+app.post('/api/mercado-livre-notifications', async (req, res) => {
+  try {
+    // Reenvia os dados para o backend Flask
+    await axios.post('https://b54e-45-163-2-79.ngrok-free.app/notifications', req.body);
+    res.status(200).send('OK');
+  } catch (error) {
+    console.error('Erro ao processar a notificação:', error);
+    res.status(500).send('Erro ao processar a notificação');
+  }
+});
+
+// Rota padrão para a página inicial
+app.get('/', (req, res) => {
+  res.send('API está rodando!');
+});
+
+// if (process.env.NODE_ENV !== 'production') {
+//     const port = process.env.PORT || 3000;
+//     app.listen(port, () => {
+//       console.log(`Servidor rodando na porta ${port}`);
+//     });
+//   }
+
+  
+// Exporta o app para a Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
